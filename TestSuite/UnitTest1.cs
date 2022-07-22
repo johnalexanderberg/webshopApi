ff7485feaa8c76cfd41d345b2b504c3b6a31772e
alexanderberg <alexander.berg1@learnet.se>
2022-07-22 13:52:39 +0200

    added project for testing

---
+++ TestSuite/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using webshopApi.Models;
+
+namespace TestSuite;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
