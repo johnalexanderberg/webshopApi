d35eebe6eb543e0bbc23d14d5c673dcd3efd62e5
alexanderberg <alexander.berg1@learnet.se>
2022-07-22 13:05:23 +0200

    adding dbcontext and models

---
+++ webshopApi/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using webshopApi.Models;
+
+namespace webshopApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
+    {
+        
+    }
+    
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    
+}
---
+++ webshopApi/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace webshopApi.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Product> Products { get; set; } = new List<Product>();
+}
