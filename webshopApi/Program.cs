92783f123c50011385e573c9779b8c1af17d3fb6
alexanderberg <alexander.berg1@learnet.se>
2022-07-22 13:07:35 +0200

    Program.cs & config files

---
+++ webshopApi/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using webshopApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
