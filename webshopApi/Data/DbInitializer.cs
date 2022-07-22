3a2acc59dffe38d805a328ca42a7ba43b14f04b9
alexanderberg <alexander.berg1@learnet.se>
2022-07-22 13:06:13 +0200

    adding DbInitializer.cs to automatically seed the database

---
+++ webshopApi/Data/DbInitializer.cs
@@ -0,0 +1,47 @@
+using webshopApi.Models;
+
+namespace webshopApi.Data;
+
+public class DbInitializer
+{
+    public static void Initialize(AppDbContext context)
+    {
+        if (context.Products.Any())
+        {
+            return; // Database has been seeded
+        }
+
+        var categories = new Category[]
+        {
+            new Category
+            {
+                Name = "Jackets"
+            },
+            new Category
+            {
+                Name = "Shoes"
+            }
+        };
+
+        var products = new Product[]
+        {
+            
+            new Product
+            {
+                Name = "Sko 1",
+                ImageUrl = "Sko 1",
+                Price = (decimal) 1.5,
+
+            },
+            new Product
+            {
+                Name = "Jacka 1",
+                ImageUrl = "Jacka 1",
+                Price = (decimal) 2.5,
+            }
+        };
+        
+        context.Products.AddRange(products);
+        context.SaveChanges();
+    }
+}
